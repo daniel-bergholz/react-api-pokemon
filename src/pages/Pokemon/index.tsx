9bf64803aef7d19940ebacf81b7aba292b5a15ac
Daniel Bergholz <daniel@maratonas.academy>
2021-08-22 15:59:36 -0300

    feat(Pokemon): create IPokemon interface

--- src/pages/Pokemon/index.tsx
+++ src/pages/Pokemon/index.tsx
@@ -2,16 +2,25 @@ import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import api from '../../services/api'
 
+interface IPokemon {
+  height: number
+  weight: number
+  name: string
+  types: { type: { name: string } }[]
+  abilities: { ability: { name: string } }[]
+  sprites: { other: { 'official-artwork': { front_default: string } } }
+}
+
 interface IParams {
   pokemon: string
 }
 
 const Pokemon: React.FC = () => {
-  const [pokemon, setPokemon] = useState()
+  const [pokemon, setPokemon] = useState<IPokemon>()
   const { pokemon: pokemonName } = useParams<IParams>()
 
   const getPokemonInfo = async () => {
-    const { data } = await api.get(`pokemon/${pokemonName}`)
+    const { data } = await api.get<IPokemon>(`pokemon/${pokemonName}`)
 
     setPokemon(data)
   }
